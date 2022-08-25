f75d3b5bf410f1faab8707ce8ff0762a6f80b57d
Jennings Caraway <82488956+jcaraway-na@users.noreply.github.com>
2022-08-25 18:53:24 -0500

    Update plot.js

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,31 +1,44 @@
 var w = 410;
 var h = 400;
 
-function makeplot() {
-    d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv", function(data){ processData(data) } );
-  
+function makeplotRigData() {
+    d3.csv("https://raw.githubusercontent.com/jcaraway-na/Nsight_ETL/main/resources/Optimal_MSE_Frankenstein.csv", function(data){ processData(data,'HoleDepth','ROPFast') } );
+    d3.csv("https://raw.githubusercontent.com/jcaraway-na/Nsight_ETL/main/resources/Optimal_MSE_Roadmap.csv", function(data){ processData(data) } );
   };
   
-  function processData(allRows) {
+  function processData(allRows,holdepth,rop,mse,wob,bitrpm) {
   
     console.log(allRows);
     var x = [], y = [], standard_deviation = [];
   
     for (var i=0; i<allRows.length; i++) {
       row = allRows[i];
-      x.push( row['AAPL_x'] );
-      y.push( row['AAPL_y'] );
+      x.push( row[holdepth] );
+      yRop.push( row[rop] );
+      yMse.push( row[mse] );
+      yWob.push( row[wob] );
+      yBitRpm.push( row[bitrpm] );
     }
-    makePlotly( x, y, standard_deviation );
+    makePlotly( x, yRop,yMse,yWob,yBitRpm, standard_deviation );
   }
   
-  function makePlotly( x, y, standard_deviation ){
+  function makePlotly( x, rop,mse,wob,bitrpm, standard_deviation ){
     var plotDiv = document.getElementById("rop-plot");
 
-    var traces = [{
+    var traceRop = [{
       x: x,
-      y: y,
-      automargin: true,
+      y: rop,
+      type: 'scatter',
+      marker: {
+        color: 'rgb(135, 61, 255)',
+        size: 8
+    }, 
+    }];
+
+    var traceMse = [{
+      x: x,
+      y: mse,
+      type: 'scatter',
       marker: {
         color: 'rgb(135, 61, 255)',
         size: 8
@@ -33,8 +46,8 @@ function makeplot() {
     }];
 
     var layout = {
-        xaxis: {title: "Days"},
-        yaxis: {title: "Depth"},
+        xaxis: {title: "Hole Depth"},
+        yaxis: {title: "ROP"},
         paper_bgcolor: 'rgba(221,221,221,1)',
         plot_bgcolor: 'rgba(221,221,221,1)',
         height: 300,
@@ -51,9 +64,9 @@ function makeplot() {
     displayModeBar: true,
     scrollZoom: true};
 
-    Plotly.newPlot('rop-plot', traces,layout,config);
+    Plotly.newPlot('rop-plot', traceRop,layout,config);
     Plotly.newPlot('mse-plot', traces,layout,config);
     Plotly.newPlot('wob-plot', traces,layout,config);
     Plotly.newPlot('bit-rpm-plot', traces,layout,config);
   };
-    makeplot();
+  makeplotRigData();
