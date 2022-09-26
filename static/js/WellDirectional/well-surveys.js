2a980be0361816915a99d656b6217f3eb71e26ba
Jennings Caraway <82488956+jcaraway-na@users.noreply.github.com>
2022-09-26 14:27:16 -0500

    update

--- static/js/WellDirectional/well-surveys.js
+++ static/js/WellDirectional/well-surveys.js
@@ -1,7 +1,9 @@
 import { getSurveyDataByWellId } from '../ApiCalls/calls.js';
-import { makePlotlySurvey, addToPlotly } from '../global-chart.js';
+import { makePlotlySurvey, addToPlotlySurvey } from '../global-chart.js';
 
 let loader = document.querySelector(".loader-big");
+let dropdown = document.getElementById('selDataset');
+
 loader.style.display = "flex";
 loader.style.height = '100vh'
 loader.style.width = '100vw'
@@ -11,7 +13,6 @@ const token = autho.token;
 const well = JSON.parse(sessionStorage.getItem('selectedWell'));
 const wellId = well.id;
 const offsetWells = JSON.parse(sessionStorage.getItem('wellList'));
-// const offset = well.id;
 console.log(offsetWells);
 
 // Header info
@@ -20,7 +21,7 @@ document.getElementById('afenumber').innerHTML = well.afeNumber;
 document.getElementById('apinumber').innerHTML = well.apiNumber;
 document.getElementById('county').innerHTML = well.county;
 document.getElementById('field').innerHTML = well.field;
-document.getElementById('lat').innerHTML = well.lat;4444444444444444444
+document.getElementById('lat').innerHTML = well.lat;
 document.getElementById('leasename').innerHTML = well.leaseName;
 document.getElementById('long').innerHTML = well.long;
 document.getElementById('permitnumber').innerHTML = well.permitNumber;
@@ -29,19 +30,29 @@ document.getElementById('targetformation').innerHTML = well.targetFormation;
 document.getElementById('wellid').innerHTML = well.id;
 // end Header info
 
-let surveyData = await getSurveyDataByWellId(wellId,token);
+let surveyData = await getSurveyDataByWellId(wellId, token);
+
+function init(data) {
+    let option;
+    for (let i = 0; i < data.length; i++) {
+        option = document.createElement('option');
+        option.text = data[i].wellName;
+        option.value = data[i].wellId;
+        dropdown.add(option);
+    }
+}
 
 // Get Subject well survey data.
-async function loadSubjectSurveys(data){
-    
+async function loadSubjectSurveys(data) {
+
     let over = 'style="color: white; background-color:rgba(255,0,0,.7);"'
     let under = ''
     for (var i in data) {
         var tdOver;
-        if(data[i].doglegSeverity > 13){
+        if (data[i].doglegSeverity > 13) {
             tdOver = over;
         }
-        else{
+        else {
             tdOver = under;
         }
         var row = `<tr>
@@ -59,70 +70,16 @@ async function loadSubjectSurveys(data){
     }
 }
 
-// async function makeSurveyPlot(data){
-//     var i, r;
-//     var x = [];
-//     var y = [];
-//     var z = [];
-//     var c = [];
 
-//     for(i = 0; i < data.length; i++){
-//         // r = 10 * Math.cos(i/10);
-//         x.push(data[i].localEastWest);
-//         y.push(data[i].localNorthSouth);
-//         z.push(-1*data[i].tvd);
-//         c.push(data[i].doglegSeverity);
-//     }
-
-//     let trace = [{
-//         type: 'scatter3d',
-//         mode: 'lines',
-//         x: x,
-//         y: y,
-//         z: z,
-//         line: {
-//           width: 10,
-//           color: c,
-//           colorscale: "Portland"},
-//         marker: {
-//           size: 3.5,
-//           color: c,
-//           colorscale: "Greens",
-//           cmin: -20,
-//           cmax: 50
-//         }
-//     }];
-    
-//     var layout = {
-//         scene:{
-
-//           xaxis: {
-//            nticks: 9,
-//            range: [-1500, 1500],
-//          },
-//           yaxis: {
-//            nticks: 7,
-//            range: [0, 15000],
-//          }},
-//         paper_bgcolor: 'rgba(0,0,0,0)',
-//         plot_bgcolor: 'rgba(0,0,0,0)',
-//         autosize: true,
-//         margin: {
-//             l: 55,
-//             r: 20,
-//             b: 50,
-//             t: 0,
-
-//         },
-//     };
-    
-//     var config = { responsive: true }
-//     Plotly.newPlot('planvactual', trace, layout, config);
-
-// }
+async function changed(id) {
+    //fetch data offset surveys
+    let data = await getSurveyDataByWellId(wellId, token);
+    await addToPlotlySurvey(data);
+}
 
 await loadSubjectSurveys(surveyData);
 await makePlotlySurvey(surveyData);
+init(offsetWells);
 
 loader.style.display = "none";
 loader.style.height = '0vh'
